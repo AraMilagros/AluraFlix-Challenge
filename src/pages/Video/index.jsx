6a3e33b39f95e41c310155b31bfb425569ad33e5
AraMilagros <araapalenque@gmail.com>
2024-07-05 23:48:08 -0300

    Ajustando estilos

--- src/pages/Video/index.jsx
+++ src/pages/Video/index.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 import estilos from './estilos.module.css';
 
+//! poner en estilos.tittle(valorpsado) para los titulos de cards
+
 export default function index() {
   return (
     <div className={estilos.container}>
-        <h2>NUEVO VIDEO</h2>
-        <h4>COMPLETE EL FORMULARIO PARA CREAR UNA NUEVA TARJETA DE VIDEO</h4>
+        <h2 className={estilos.tittle}>NUEVO VIDEO</h2>
+        <h4 className={estilos.subTittle}>COMPLETE EL FORMULARIO PARA CREAR UNA NUEVA TARJETA DE VIDEO</h4>
         <hr/>
         <h3>Crear Tarjeta</h3>
         <hr />
 
-        <form>
-            <div>
+        <form className={estilos.formulario}>
+            <div className={estilos.input__text} id={estilos.input__titulo}>
                 <label>Titulo</label>
                 <input type="text" placeholder='Ingrese el título'/>
             </div>
 
-            <div>
+            <div className={estilos.input__text} id={estilos.input__categoria}>
                 <label>Categoría</label>
                 <select>
                     <option id="seleccion">Seleccione una categoria</option>
@@ -27,22 +29,22 @@ export default function index() {
                 </select>
             </div>
 
-            <div>
+            <div className={estilos.input__text} id={estilos.input__imagen}>
                 <label>Imagen</label>
                 <input type="text" placeholder='Ingrese enlace de la imagen'/>
             </div>
 
-            <div>
+            <div className={estilos.input__text} id={estilos.input__video}>
                 <label>Video</label>
-                <input type="text" pattern='Ingrese el enlace del video'/>
+                <input type="text" placeholder='Ingrese el enlace del video'/>
             </div>
 
-            <div>
+            <div className={estilos.input__text} id={estilos.input__descripcion}>
                 <label>Descripción</label>
-                <textarea></textarea>
+                <textarea rows={5} placeholder='¿De qué trata este video?'></textarea>
             </div>
 
-            <div>
+            <div className={estilos.input__text} id={estilos.input__btns}>
                 <input type="submit" value="GUARDAR"/>
                 <input type="button" value="LIMPIAR" />
             </div>
